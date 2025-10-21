ea2266bda06293ee9303b15ccb8a3aaef38aec7f
[dyad] <evettrg@gmail.com>
2025-10-21 16:47:28 +0800

    [dyad] Membetulkan ralat import Link dalam Index.tsx - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import { MadeWithDyad } from "@/components/made-with-dyad";
